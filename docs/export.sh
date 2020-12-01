404c2a0d6df53b6cd232df56325ae2a086fc6e70
jlecordier <jean.lecordier@insa-rouen.fr>
2020-12-01 21:10:22 +0100

    ajout script générer le pdf

---
+++ docs/export.sh
@@ -0,0 +1,8 @@
+pandoc -s monographie.md -o 2020A20W.20W.versionB.pdf
+#pandoc -f gfm+pipe_tables -s monographie.md -o 2020A20W.20W.versionB.pdf
+#pandoc --toc --template=miniproj.tpl -o tmp1.md monographie.md
+#pandoc -s tmp1.md -o monographie.pdf
+
+#pandoc --toc --template=miniproj.tpl -o tmp1.md monographie.md
+#pandoc -f gfm+pipe_tables -s tmp1.md -o 2020A20W.20W.versionB.pdf
+# rm tmp1.md
